4e73adc00fdadb3b98c27979cd36ce1bc0dacaa8
Sky_SuGar <wxk0218@icloud.com>
2026-05-13 19:55:41 +0800

    Add CornerRadiusHelper and CapsuleShapeHelper

---
+++ SuGarToolkit.WPF.GalleryApp/Controls/CapsuleShapeHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SuGarToolkit.WPF.GalleryApp.Controls;
+
+public class CapsuleShapeHelper
+{
+    public static void ShapeControlIntoCapsule(Control control)
+    {
+        if (VisualTreeHelper.GetChildrenCount(control) > 0 && VisualTreeHelper.GetChild(control, 0) is Border border)
+        {
+            ShapeBorderIntoCapsule(border);
+        }
+    }
+
+    public static void ShapeBorderIntoCapsule(Border border)
+    {
+        border.CornerRadius = new CornerRadius(Math.Min(border.ActualWidth, border.ActualHeight) / 2);
+    }
+
+    private static void ShapeFrameworkElementIntoCapsule(FrameworkElement element)
+    {
+        if (element is Border border)
+        {
+            ShapeBorderIntoCapsule(border);
+            return;
+        }
+        if (element is Control control)
+        {
+            ShapeControlIntoCapsule(control);
+            return;
+        }
+    }
+
+    public static bool GetShapeIntoCapsule(Control target) => (bool) target.GetValue(ShapeIntoCapsuleProperty);
+    public static void SetShapeIntoCapsule(Control target, bool value) => target.SetValue(ShapeIntoCapsuleProperty, value);
+
+    public static readonly DependencyProperty ShapeIntoCapsuleProperty = DependencyProperty.RegisterAttached(
+        "ShapeIntoCapsule",
+        typeof(bool),
+        typeof(FrameworkElement),
+        new PropertyMetadata(default(bool), OnShapeIntoCapsuleChanged)
+    );
+
+    private static void OnShapeIntoCapsuleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        FrameworkElement target = (FrameworkElement) d;
+        ShapeFrameworkElementIntoCapsule(target);
+        if ((bool) e.NewValue)
+        {
+            target.SizeChanged += OnSizeChanged;
+        }
+        else
+        {
+            target.SizeChanged -= OnSizeChanged;
+        }
+    }
+
+    private static void OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        FrameworkElement target = (FrameworkElement) sender;
+        ShapeFrameworkElementIntoCapsule(target);
+    }
+}
---
+++ SuGarToolkit.WPF.GalleryApp/Controls/CornerRadiusHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SuGarToolkit.WPF.GalleryApp.Controls;
+
+public class CornerRadiusHelper
+{
+    public static CornerRadius GetCornerRadius(FrameworkElement target) => (CornerRadius) target.GetValue(CornerRadiusProperty);
+    public static void SetCornerRadius(FrameworkElement target, CornerRadius value) => target.SetValue(CornerRadiusProperty, value);
+
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+        "CornerRadius",
+        typeof(CornerRadius),
+        typeof(Control),
+        new PropertyMetadata(default(CornerRadius), OnCornerRadiusChanged)
+    );
+
+    private static void OnCornerRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Control control)
+            return;
+
+        if (!control.IsLoaded)
+        {
+            control.Loaded += OnLoaded;
+            void OnLoaded(object sender, RoutedEventArgs args)
+            {
+                control.Loaded -= OnLoaded;
+                if (VisualTreeHelper.GetChildrenCount(control) > 0 && VisualTreeHelper.GetChild(control, 0) is Border border)
+                {
+                    border.CornerRadius = (CornerRadius) e.NewValue;
+                }
+            }
+        }
+        else if (VisualTreeHelper.GetChildrenCount(control) > 0 && VisualTreeHelper.GetChild(control, 0) is Border border)
+        {
+            border.CornerRadius = (CornerRadius) e.NewValue;
+        }
+    }
+}
