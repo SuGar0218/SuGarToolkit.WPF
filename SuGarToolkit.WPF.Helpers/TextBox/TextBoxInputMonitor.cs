242db16b890c10f240dd51eb90be92e479ef1319
Sky_SuGar <wxk0218@icloud.com>
2026-06-20 20:45:48 +0800

    Optimize TextBoxInputMonitor.Dispose

--- SuGarToolkit.WPF.Helpers/TextBox/TextBoxInputMonitor.cs
+++ SuGarToolkit.WPF.Helpers/TextBox/TextBoxInputMonitor.cs
@@ -15,7 +15,7 @@ public partial class TextBoxInputMonitor : IDisposable
         TextCompositionManager.AddPreviewTextInputHandler(TargetTextBox, OnTextCompositionComplete);
     }
 
-    public void Dispose()
+    private void PrivateDispose()
     {
         TargetTextBox.TextChanged -= OnTextBoxTextChanged;
         TextCompositionManager.RemovePreviewTextInputStartHandler(TargetTextBox, OnTextCompositionStart);
@@ -110,4 +110,40 @@ public partial class TextBoxInputMonitor : IDisposable
         InputMethodEditingComplete?.Invoke(this, EventArgs.Empty);
         TargetTextBox.RaiseEvent(new RoutedEventArgs(InputMethodEditingCompleteEvent));
     }
+
+    #region IDisposable
+
+    private bool disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: 释放托管状态(托管对象)
+                PrivateDispose();
+            }
+
+            // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            // TODO: 将大型字段设置为 null
+            disposedValue = true;
+        }
+    }
+
+    // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+    // ~TextBoxInputMonitor()
+    // {
+    //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
 }
