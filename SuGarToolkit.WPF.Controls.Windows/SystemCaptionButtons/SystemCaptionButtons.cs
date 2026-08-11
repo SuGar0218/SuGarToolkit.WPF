b79ef828c1b0a1fa4dc97bafb3cf22db45b74889
Sky_SuGar <wxk0218@icloud.com>
2026-08-11 21:38:46 +0800

    Add SystemCaptionButtons attached properties for Window

---
+++ SuGarToolkit.WPF.Controls.Windows/SystemCaptionButtons/SystemCaptionButtons.cs
@@ -0,0 +1,99 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace SuGarToolkit.WPF.Controls.Windows;
+
+public class SystemCaptionButtons
+{
+    public static bool GetCanMinimize(Window target) => (bool)target.GetValue(CanMinimizeProperty);
+    public static void SetCanMinimize(Window target, bool value) => target.SetValue(CanMinimizeProperty, value);
+
+    public static readonly DependencyProperty CanMinimizeProperty = DependencyProperty.RegisterAttached(
+        "CanMinimize",
+        typeof(bool),
+        typeof(SystemCaptionButtons),
+        new PropertyMetadata(true, OnCanMinimizeChanged)
+    );
+
+    private static void OnCanMinimizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        Window window = (Window)d;
+        bool can = (bool)e.NewValue;
+        nint handle = RetrieveWindowHandle(window);
+        if (handle != nint.Zero)
+        {
+            Win32SystemCaptionButtons.SetCanMinimize(handle, can);
+            return;
+        }
+        ExecuteOnceAfterSourceInitialized(window, () =>
+        {
+            Win32SystemCaptionButtons.SetCanMinimize(handle, can);
+        });
+    }
+
+    public static bool GetCanMaximize(Window target) => (bool)target.GetValue(CanMaximizeProperty);
+    public static void SetCanMaximize(Window target, bool value) => target.SetValue(CanMaximizeProperty, value);
+
+    public static readonly DependencyProperty CanMaximizeProperty = DependencyProperty.RegisterAttached(
+        "CanMaximize",
+        typeof(bool),
+        typeof(SystemCaptionButtons),
+        new PropertyMetadata(true, OnCanMaximizeChanged)
+    );
+
+    private static void OnCanMaximizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        Window window = (Window)d;
+        bool can = (bool)e.NewValue;
+        nint handle = RetrieveWindowHandle(window);
+        if (handle != nint.Zero)
+        {
+            Win32SystemCaptionButtons.SetCanMaximize(handle, can);
+            return;
+        }
+        ExecuteOnceAfterSourceInitialized(window, () =>
+        {
+            Win32SystemCaptionButtons.SetCanMaximize(handle, can);
+        });
+    }
+
+    public static bool GetIsVisible(Window target) => (bool)target.GetValue(IsVisibleProperty);
+    public static void SetIsVisible(Window target, bool value) => target.SetValue(IsVisibleProperty, value);
+
+    public static readonly DependencyProperty IsVisibleProperty = DependencyProperty.RegisterAttached(
+        "IsVisible",
+        typeof(bool),
+        typeof(SystemCaptionButtons),
+        new PropertyMetadata(true, OnIsVisibleChanged)
+    );
+
+    private static void OnIsVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        Window window = (Window)d;
+        bool visible = (bool)e.NewValue;
+        nint handle = RetrieveWindowHandle(window);
+        if (handle != nint.Zero)
+        {
+            Win32SystemCaptionButtons.SetIsVisible(handle, visible);
+            return;
+        }
+        ExecuteOnceAfterSourceInitialized(window, () =>
+        {
+            Win32SystemCaptionButtons.SetIsVisible(handle, visible);
+        });
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static nint RetrieveWindowHandle(Window window) => new WindowInteropHelper(window).Handle;
+
+    private static void ExecuteOnceAfterSourceInitialized(Window window, Action action)
+    {
+        window.SourceInitialized += ExecuteActionOnSourceInitialized;
+        void ExecuteActionOnSourceInitialized(object? sender, EventArgs e)
+        {
+            window.SourceInitialized -= ExecuteActionOnSourceInitialized;
+            action.Invoke();
+        }
+    }
+}
---
+++ SuGarToolkit.WPF.Controls.Windows/SystemCaptionButtons/Win32SystemCaptionButtons.cs
@@ -0,0 +1,78 @@
+﻿using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace SuGarToolkit.WPF.Controls.Windows;
+
+public class Win32SystemCaptionButtons
+{
+    public static bool GetCanMaximize(nint hwnd)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        return (style & WS_MAXIMIZEBOX) != 0;
+    }
+
+    public static int SetCanMaximize(nint hwnd, bool value)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        if (value)
+        {
+            style |= WS_MAXIMIZEBOX;
+        }
+        else
+        {
+            style &= ~WS_MAXIMIZEBOX;
+        }
+        return PInvoke.SetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
+
+    public static bool GetCanMinimize(nint hwnd)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        return (style & WS_MINIMIZEBOX) != 0;
+    }
+
+    public static int SetCanMinimize(nint hwnd, bool value)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        if (value)
+        {
+            style |= WS_MINIMIZEBOX;
+        }
+        else
+        {
+            style &= ~WS_MINIMIZEBOX;
+        }
+        return PInvoke.SetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
+
+    public static bool GetIsVisible(nint hwnd)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        return (style & WS_SYSMENU) != 0;
+    }
+
+    public static int SetIsVisible(nint hwnd, bool value)
+    {
+        HWND handle = new(hwnd);
+        int style = PInvoke.GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        if (value)
+        {
+            style |= WS_SYSMENU;
+        }
+        else
+        {
+            style &= ~WS_SYSMENU;
+        }
+        return PInvoke.SetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
+
+    private const int WS_MAXIMIZEBOX = 0x00010000;
+    private const int WS_MINIMIZEBOX = 0x00020000;
+    private const int WS_SYSMENU = 0x00080000;
+}
